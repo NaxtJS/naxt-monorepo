c51d65fc6c2949a83f912c7f8013dd910fa07bcd
veysa <e>
2022-05-14 12:34:44 +0400

    chore(utils): change extension & get extension

--- packages/utils/src/path/extension.ts
+++ packages/utils/src/path/extension.ts
@@ -15,4 +15,17 @@ export class Extension {
   isSameToOneOf(extensions: string[]): boolean {
     return false;
   }
+
+  setExt(extension: string) {
+    this.path.path = this.path.path.replace(
+      new RegExp(`${extname(this.path.path)}$`),
+      extension.startsWith(".") ? extension : `.${extension}`
+    );
+
+    return this.path;
+  }
+
+  toString() {
+    return extname(this.path.fullPath).slice(1);
+  }
 }
