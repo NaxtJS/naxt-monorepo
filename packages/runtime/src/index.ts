bdd5fab6d7288dcb4f474115c2efab0772bff9fc
veysa <e>
2022-05-14 12:28:47 +0400

    chore(runtime): collect main to runtime

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -6,4 +6,7 @@ export const config = new Config<BaseConfig>();
 export const naxt = (naxtConfig: NaxtConfig) => new Naxt(naxtConfig);
 
 export { defaultAppConfig } from "@naxt/core";
-export { resolveConfig } from "@naxt/utils";
+export { resolveConfig, Path, getPages } from "@naxt/utils";
+export { NaxtConfig } from "@naxt/types";
+export { parse } from "@naxt/parser";
+export { generate } from "@naxt/generator";
