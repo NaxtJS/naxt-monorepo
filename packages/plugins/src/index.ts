201b6774d0ea4b541fd0d447fb7d8ffb346a8bc4
veysa <e>
2022-05-14 12:28:08 +0400

    chore(plugins): resolve rollup plugins

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -1,1 +1,14 @@
-export const resolvePlugins = () => {};
+import { Plugin } from "rollup";
+import { config } from "@naxt/runtime";
+import {} from "typescript";
+
+// plugins
+import typescript from "@rollup/plugin-typescript";
+
+export const resolvePlugins = (): Plugin[] => {
+  const rootTsConfigFile = config.getConfig("appRoot").duplicateTo("tsconfig.json");
+  const tsConfig = rootTsConfigFile.exists ? rootTsConfigFile.source.readAsJSON() : {};
+  tsConfig.module = "esnext";
+
+  return [typescript({ ...tsConfig })];
+};
