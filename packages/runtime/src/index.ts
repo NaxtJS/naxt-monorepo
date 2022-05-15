eb2909cf0eede7da222857f1d3f30f80e790029f
veysa <e>
2022-05-15 19:42:24 +0400

    chore: runtime exports

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -4,9 +4,11 @@ import { BaseConfig, NaxtConfig } from "@naxt/types";
 export const config = new Config<BaseConfig>();
 
 export const naxt = (naxtConfig: NaxtConfig) => new Naxt(naxtConfig);
+export const NULL_CHAR = String.fromCharCode(0x0000);
 
 export { defaultAppConfig } from "@naxt/core";
 export { resolveConfig, Path, getPages } from "@naxt/utils";
 export { NaxtConfig } from "@naxt/types";
 export { parse } from "@naxt/parser";
 export { generate } from "@naxt/generator";
+export { resolvePlugins } from "@naxt/plugins";
