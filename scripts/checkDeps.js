f121848164946460d8b607b484cd78127f889a33
Veys Aliyev <40066034+mdreal32@users.noreply.github.com>
2022-05-18 08:17:10 +0400

    refactor: removed unused parts

--- scripts/checkDeps.js
+++ scripts/checkDeps.js
@@ -1,5 +1,4 @@
 /**
- *
  * @typedef {{
  *    dependencies: string[],
  *    devDependencies: string[],
@@ -10,44 +9,55 @@
  *  }} DepCheck
  */
 
-const { resolve, basename } = require("path");
-const glob = require("glob");
-const EventEmitter = require("events");
+const { resolve, dirname } = require("path");
+const { writeFileSync, mkdirSync, unlinkSync } = require("fs");
 
-const { workspaces } = require("../package.json");
-const { writeFileSync } = require("fs");
+const isDevDep = dep => dep.startsWith("@types") || dep === "@naxt/types";
 
-const packages = [".", ...workspaces]
-  .map(workspace => glob.sync(workspace, { absolute: true }))
-  .flat()
-  .map(pkg => {
-    const pkgJsonFile = resolve(pkg, "package.json");
-    const pkgJson = require(pkgJsonFile);
-    return { root: pkgJsonFile, pkg: pkgJson };
-  });
+process.stdin.on(
+  "data",
+  /** @param {Buffer} data */ data => {
+    const cleanData = data
+      .toString("utf-8")
+      .replace(/➤ YN\d{0,4}:/g, "")
+      .trim();
 
-const missingPackages = {};
-process.stdin
-  .on(
-    "data",
-    /** @param {Buffer} data */ data => {
-      const cleanData = data
-        .toString("utf-8")
-        .replace(/➤ YN\d{0,4}:/g, "")
-        .trim();
+    try {
+      const [, repo, matchedData] = cleanData?.match(/^\[([\s\S]+?)]:\s*([\s\S]+?)$/);
+      /** @type {DepCheck} */
+      const { missing } = JSON.parse(matchedData);
+      const dependencies = Object.keys(missing);
+      const pkgName = repo.slice(repo.startsWith("@") ? 1 : 0).replace(/\W+/g, "-");
+      const missingDepFile = resolve(process.cwd(), "missingRepos", `${pkgName}.json`);
 
       try {
-        const [, repo, matchedData] = cleanData?.match(/^\[([\s\S]+?)]:\s*([\s\S]+?)$/);
-        /** @type {DepCheck} */
-        const { missing } = JSON.parse(matchedData);
-        const dependencies = Object.keys(missing);
-        const pkgName = repo.slice(repo.startsWith("@") ? 1 : 0).replace(/\W+/g, "-");
-        const missingDepFile = resolve(process.cwd(), "missingRepos", `${pkgName}.json`);
-        writeFileSync(missingDepFile, JSON.stringify({ dependencies }, null, 2));
-        console.log(`Wrote missing dependencies to file: ${missingDepFile}`);
-      } catch (e) {
-        console.log(cleanData);
+        unlinkSync(missingDepFile);
+      } catch (e) {}
+      if (dependencies.length) {
+        mkdirSync(dirname(missingDepFile), { recursive: true });
+        const deps = dependencies.filter(dep => !isDevDep(dep));
+        const devDeps = dependencies.filter(isDevDep);
+
+        writeFileSync(
+          missingDepFile,
+          JSON.stringify(
+            {
+              dependencies,
+              commands: {
+                dep: (deps.length && `yarn workspace ${repo} add ${deps.join(" ")}`) || undefined,
+                dev:
+                  (devDeps.length && `yarn workspace ${repo} add --dev ${devDeps.join(" ")}`) ||
+                  undefined
+              }
+            },
+            null,
+            2
+          )
+        );
+        console.log(`[${repo}]: Wrote missing dependencies to file: ${missingDepFile}`);
       }
+    } catch (e) {
+      console.log(cleanData);
     }
-  )
-  .on("end", () => {});
+  }
+);
