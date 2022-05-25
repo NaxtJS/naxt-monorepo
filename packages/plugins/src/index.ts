7c186d5ad3a9f30488308e3aaa1f5831a25fadd1
MDReal32 <veysaliyev00@gmail.com>
2022-05-25 12:15:09 +0400

    chore: moved to internal plugin

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -1,19 +1,7 @@
 import { Plugin } from "rollup";
-import { config } from "@naxt/runtime";
-
-// Internal Plugins
 import { worker } from "./workerPlugin";
-
-// External plugins
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
+import { typescript } from "./plugins/typescript";
 
 export const resolvePlugins = (): Plugin[] => {
-  const rootTsConfigFile = config.getConfig("appRoot").duplicateTo("tsconfig.json");
-  const tsConfig = rootTsConfigFile.exists ? rootTsConfigFile.source.readAsJSON() : {};
-  tsConfig.compilerOptions ||= {};
-  tsConfig.compilerOptions.module = "ESNext";
-  tsConfig.compilerOptions.target = "ESNext";
-
-  return [typescript({ ...tsConfig }), worker(), worker.post()];
+  return [typescript(), worker(), worker.post()];
 };
