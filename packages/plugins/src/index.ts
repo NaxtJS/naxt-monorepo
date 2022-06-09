b184b5f62923da63885e5ed6d344d1e0faa99401
MDReal32 <veysaliyev00@gmail.com>
2022-06-09 17:56:45 +0400

    chore: export constants

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -5,3 +5,6 @@ import { typescript } from "./plugins/typescript";
 export const resolvePlugins = (): Plugin[] => {
   return [typescript(), worker(), worker.post()];
 };
+
+export const ENTRYPOINT_BASENAME = "naxt:entry";
+export const NULL_CHAR = String.fromCharCode(0x00);
