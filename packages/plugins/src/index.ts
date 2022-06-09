70c7a88d001697e0561ec1e09bf0e2e794121c0c
MDReal32 <veysaliyev00@gmail.com>
2022-06-09 17:57:53 +0400

    chore: plugins reworked
    @rollup/plugin-node-resolve and @rollup/plugin-commonjs added. Other plugins updated

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -3,7 +3,17 @@ import { worker } from "./workerPlugin";
 import { typescript } from "./plugins/typescript";
 
 export const resolvePlugins = (): Plugin[] => {
-  return [typescript(), worker(), worker.post()];
+  const isBuild = config.getConfig("isBuild");
+  const plugins: Plugin[] = [];
+
+  plugins.push(typescript());
+  plugins.push(worker());
+  plugins.push(nodeResolve({ browser: true }));
+  plugins.push(commonjs());
+  plugins.push(worker.post());
+  // plugins.push(terser());
+
+  return plugins;
 };
 
 export const ENTRYPOINT_BASENAME = "naxt:entry";
