0a32400450f7f446548f4cc88b8584914f714c36
MDReal32 <veysaliyev00@gmail.com>
2022-06-10 16:47:52 +0400

    chore(demo): styles & media(image) support added

--- packages/demo/pages/index.js
+++ packages/demo/pages/index.js
@@ -1,5 +1,8 @@
 import { add } from "lodash";
+import styles from "../assets/style.module.css";
+import "../assets/style.css";
 
 export default () => {
   console.log(add(1, 2));
+  console.log(styles.graph);
 };
--- packages/demo/pages/ts.ts
+++ packages/demo/pages/ts.ts
@@ -1,7 +1,11 @@
 import { useCalculation } from "../hooks/useCalculation";
+// @ts-ignore
+import image from "../assets/index.jpg";
 
 export default () => {
   const string: string = "Hello World";
   console.log(string);
   console.log(useCalculation(1, 2));
+
+  console.log(image);
 };
