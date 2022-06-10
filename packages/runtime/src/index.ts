a918890b8e1c7f2b00e3502242fdf2b082e47e71
MDReal32 <veysaliyev00@gmail.com>
2022-06-10 17:25:15 +0400

    chore(runtime): export module from types

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -9,4 +9,5 @@ export const NULL_CHAR = String.fromCharCode(0x0000);
 export { defaultAppConfig } from "@naxt/core";
 export { resolveConfig, Path, getPages, StringBuilder, generateHash } from "@naxt/utils";
 export { NaxtConfig, Plugin, Logger, LoggerLevels } from "@naxt/types";
+export { ModuleGraphItem, ModuleGraph as ModuleGraphType } from "@naxt/types";
 export { resolvePlugins, WorkerPlugin, ENTRYPOINT_BASENAME } from "@naxt/plugins";
