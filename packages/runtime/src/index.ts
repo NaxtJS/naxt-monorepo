900b8f801733f039a09f288aa66aeb9178270d20
MDReal32 <veysaliyev00@gmail.com>
2022-06-10 17:19:37 +0400

    build(runtime): export generateHash from utils

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -7,6 +7,6 @@ export const naxt = (naxtConfig: NaxtConfig) => new Naxt(naxtConfig);
 export const NULL_CHAR = String.fromCharCode(0x0000);
 
 export { defaultAppConfig } from "@naxt/core";
-export { resolveConfig, Path, getPages, StringBuilder } from "@naxt/utils";
+export { resolveConfig, Path, getPages, StringBuilder, generateHash } from "@naxt/utils";
 export { NaxtConfig, Plugin, Logger, LoggerLevels } from "@naxt/types";
 export { resolvePlugins, worker, ENTRYPOINT_BASENAME } from "@naxt/plugins";
