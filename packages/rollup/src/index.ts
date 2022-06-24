b0d806a57035033c2fcb63163f24d8a48cb9a7fa
MDReal32 <veysaliyev00@gmail.com>
2022-06-24 12:42:37 +0400

    chore: only save chunk and assets.
    Other things will be pushed as asset/chunk

--- packages/rollup/src/index.ts
+++ packages/rollup/src/index.ts
@@ -32,8 +32,7 @@ export const generate = async (bundle: RollupBuild, outputOptions: OutputOptions
   for (const outputOption of outputOptions) {
     const { output } = await bundle.generate(outputOption);
 
-    for (let chunk of output) {
-      const isEntry = chunk.type === "chunk" && chunk.isEntry;
+    for (const chunk of output) {
       const source =
         chunk.type === "chunk"
           ? chunk.code
@@ -42,39 +41,9 @@ export const generate = async (bundle: RollupBuild, outputOptions: OutputOptions
             ? chunk.source
             : new TextDecoder("utf-8").decode(chunk.source)
           : "";
-      const hash = generateHash(source);
-      const fileExtension = Path.from(chunk.fileName).extension.toString();
-      const fileName = chunk.name?.replace(/^_|_$/g, "") || "";
 
-      const filename = isEntry ? `${fileName}.${hash}.${fileExtension}` : chunk.fileName;
-      const assetName = `assets/${filename}`;
-      const path = Path.from(assetName, appConfig.build.dir);
-      path.source.saveFile(source);
-
-      if (isEntry) {
-        const html = new StringBuilder()
-          .append(`<!DOCTYPE html>`)
-          .append(`<html lang="en">`)
-          .append(`  <head>`)
-          // ToDo: get title from page.
-          .append(`    <title></title>`)
-          // .appendGroup(cssAsset => `    <link rel="stylesheet" href="/${cssAsset}" />`, cssAssets)
-          .append(`  </head>`)
-          .append()
-          .append(`  <body>`)
-          .append(`    <div id="#app">`)
-          // .append(`      ${htmlContent}`, !!htmlContent)
-          .append(`    </div>`)
-          .append()
-          .append(`    <script type="module" src="/${assetName}"></script>`, !!assetName)
-          .append(`  </body>`)
-          .append(`</html>`)
-          .build();
-
-        Path.from(chunk.fileName, appConfig.build.dir)
-          .extension.setExt("html")
-          .source.saveFile(html);
-      }
+      const filePath = Path.from(chunk.fileName, appConfig.build.dir);
+      filePath.source.saveFile(source);
     }
   }
 };
