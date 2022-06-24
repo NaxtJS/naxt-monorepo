2f8336bcfdc9824a7a28ebf9f3339acd67a32397
MDReal32 <veysaliyev00@gmail.com>
2022-06-24 12:54:08 +0400

    chore: handles metadata for each chunk

---
+++ packages/plugins/src/plugins/chunkMetadataPlugin.ts
@@ -0,0 +1,19 @@
+import { Plugin } from "@naxt/runtime";
+
+export function chunkMetadataPlugin(): Plugin {
+  return {
+    name: "naxt-chunk-build-metadata-plugin",
+
+    renderChunk(_code, chunk) {
+      chunk.naxtMetadata = {};
+      chunk.getMetadata = module => {
+        return (
+          chunk.naxtMetadata[module] ||
+          (chunk.naxtMetadata[module] = { importedAssets: new Set(), importedCss: new Set() })
+        );
+      };
+
+      return null;
+    }
+  };
+}
