0b63732ab583d17c556ca54ac1c4382f74f7ff04
MDReal32 <veysaliyev00@gmail.com>
2022-06-24 12:51:02 +0400

    chore: WIP: Will be implemented in future

---
+++ packages/plugins/src/plugins/preAlias.ts
@@ -0,0 +1,19 @@
+import { Plugin } from "@naxt/runtime";
+
+/**
+ * ToDO: WIP: Will be implemented in future
+ * @link https://github.com/vitejs/vite/blob/main/packages/vite/src/node/plugins/preAlias.ts
+ */
+export function preAlias(): Plugin {
+  return {
+    name: "naxt-pre-alias-plugin",
+
+    async resolveId(id, importer, options) {
+      // const ssr = options?.ssr ?? false;
+      // const depsOptimizer = getDepsOptimizer(config);
+      // if (depsOptimizer && bareImportRE.test(id) && !options?.scan) {
+      //   return await tryOptimizedResolve(depsOptimizer, ssr, id, importer);
+      // }
+    }
+  };
+}
