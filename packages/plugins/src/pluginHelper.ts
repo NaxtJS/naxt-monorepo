7dd88ad2f2f90ed8d0aade60597ad2519c78488b
MDReal32 <veysaliyev00@gmail.com>
2022-06-24 12:40:23 +0400

    chore: rename from old to new

---
+++ packages/plugins/src/pluginHelper.ts
@@ -0,0 +1,22 @@
+import { Path } from "@naxt/utils";
+import { ENTRYPOINT_BASENAME, NULL_CHAR } from "./index";
+import { config } from "@naxt/runtime";
+
+export class PluginHelper {
+  static transformToInputFile(file: string | Path) {
+    return `${NULL_CHAR}${ENTRYPOINT_BASENAME}?page=${file instanceof Path ? file.fullPath : file}`;
+  }
+
+  static cleanInputFile(input: string): Path | null {
+    if (
+      input.startsWith(NULL_CHAR + ENTRYPOINT_BASENAME) ||
+      input.startsWith(ENTRYPOINT_BASENAME)
+    ) {
+      const appRoot = config.getConfig("appRoot");
+      const path = Path.from<{ page: string }>(input);
+      return Path.from(path.query.page).assignRoot(appRoot.duplicateTo("pages"));
+    }
+
+    return null;
+  }
+}
