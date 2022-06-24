46f908ca0b6fab4ba7cc3326179e73b2bdf3ed2c
MDReal32 <veysaliyev00@gmail.com>
2022-06-24 12:39:07 +0400

    chore: update assetname

--- packages/plugins/src/plugins/media.ts
+++ packages/plugins/src/plugins/media.ts
@@ -20,9 +20,9 @@ export const media = (): Plugin => {
 
       if (sourcePath.extension.isSameToOneOf(mimeTypes)) {
         const hash = generateHash(sourcePath.source.readAsBase64());
-        const asset = `assets/${sourcePath.basename}.${hash}.${sourcePath.extension}`;
+        const asset = `${sourcePath.basename}.${hash}.${sourcePath.extension}`;
         assets.set(sourcePath.fullPath, asset);
-        return `export default "/${asset}"`;
+        return `export default "/assets/${asset}"`;
       }
     },
 
@@ -30,13 +30,8 @@ export const media = (): Plugin => {
       const { build } = config.getConfig("appConfig");
 
       assets.forEach((destination, source) => {
-        Path.from(source).copyTo(Path.from(destination, build.dir));
+        Path.from(source).copyTo(Path.from(`assets/${destination}`, build.dir));
       });
-
-      /*this.emitFile({
-          type: "asset",
-          name: `${sourcePath.basename}.${hash}.${sourcePath.extension}`
-        });*/
     }
   };
 };
