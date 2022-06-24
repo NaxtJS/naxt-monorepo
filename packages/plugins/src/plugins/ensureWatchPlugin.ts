d150062567b1c4c560d46d106ce98fca70ec4999
MDReal32 <veysaliyev00@gmail.com>
2022-06-24 12:53:08 +0400

    chore: json plugin

---
+++ packages/plugins/src/plugins/ensureWatchPlugin.ts
@@ -0,0 +1,10 @@
+import { Plugin } from "@naxt/runtime";
+
+/**
+ * ToDo: WIP: Will be implemented in future
+ */
+export const ensureWatchPlugin = (): Plugin => {
+  return {
+    name: "naxt-ensure-watch-plugin"
+  };
+};
