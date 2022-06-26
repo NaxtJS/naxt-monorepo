185081c641f0e7291c1c10d4145db74ca21a748c
MDReal32 <veysaliyev00@gmail.com>
2022-06-26 23:20:44 +0400

    refactor: terser plugin

---
+++ packages/plugins/src/plugins/terserPlugin.ts
@@ -0,0 +1,25 @@
+import { config, Plugin } from "@naxt/runtime";
+import MagicString from "magic-string";
+
+export const terserPlugin = (): Plugin => {
+  const appConfig = config.getConfig("appConfig");
+  const terserOptions =
+    typeof appConfig.build.terserOptions === "boolean" ? {} : appConfig.build.terserOptions;
+
+  return {
+    name: "naxt-terser-plugin",
+
+    async renderChunk(code, chunk, outputOptions) {
+      const terser = require("terser");
+      const minifiedCode = await terser.minify(code, {
+        safari10: true,
+        ...terserOptions,
+        sourceMap: !!outputOptions.sourcemap,
+        module: outputOptions.format.startsWith("es"),
+        toplevel: outputOptions.format === "cjs"
+      });
+      const ms = new MagicString(minifiedCode.code);
+      return { code: ms.toString(), map: ms.generateMap({ hires: true }) };
+    }
+  };
+};
