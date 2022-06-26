e2c3b1f9f48597663c38dcd9d39e63caa4f1676d
MDReal32 <veysaliyev00@gmail.com>
2022-06-26 23:23:51 +0400

    chore: export generate html

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,6 +3,7 @@ export * from "./builders";
 export * from "./plugins";
 
 export * from "./generateHash";
+export * from "./generateHtml";
 export * from "./getPages";
 export * from "./resolveConfig";
 export * from "./stripBOM";
