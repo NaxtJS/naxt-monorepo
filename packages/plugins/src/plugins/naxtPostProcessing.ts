248b35a7bc80e15724bf5839ae4e8bab8d9d2582
MDReal32 <veysaliyev00@gmail.com>
2022-06-26 23:20:21 +0400

    refactor: simplified

--- packages/plugins/src/plugins/naxtPostProcessing.ts
+++ packages/plugins/src/plugins/naxtPostProcessing.ts
@@ -1,19 +1,23 @@
-import type { Plugin, ChunkMetadata } from "@naxt/runtime";
+import type { Plugin } from "@naxt/runtime";
 import { ENTRYPOINT_BASENAME, PluginHelper, StringBuilder } from "@naxt/runtime";
 import { NULL_CHAR } from "..";
+import MagicString from "magic-string";
+import { generateHtml } from "@naxt/utils";
 
 export const naxtPostProcessing = (): Plugin => {
-  const licenseRegex = /\/\*\*\n\s*\*\s*@license[\s\S]*?\*\//;
+  const licenseRegex = /\/\*\*\n\s*\*\s*@license[\s\S]*?\*\//g;
   const license = new StringBuilder();
 
   return {
     name: "naxt-post-processing-plugin",
 
     transform(code) {
-      let m;
-      while ((m = code.match(licenseRegex))) {
-        code = code.replace(licenseRegex, "");
-        license.append(m[0]);
+      const msCode = new MagicString(code);
+      let found = false;
+      for (const [m] of code.matchAll(licenseRegex)) {
+        msCode.replace(licenseRegex, "");
+        license.append(m);
+        found = true;
       }
       if (found) return { code: msCode.toString(), map: msCode.generateMap({ hires: true }) };
     },
@@ -22,39 +26,12 @@ export const naxtPostProcessing = (): Plugin => {
       if (chunk.facadeModuleId?.startsWith(NULL_CHAR + ENTRYPOINT_BASENAME)) {
         const entryPoint = chunk.getEntrypoint();
         const page = PluginHelper.cleanInputFile(entryPoint);
-        const meta: ChunkMetadata = chunk.getMetadata(entryPoint);
+        const html = generateHtml(chunk.fileName, chunk.getMetadata(entryPoint));
 
         this.emitFile({
           type: "asset",
           fileName: page.duplicateTo("", false).extension.set("html").importPath,
-          source: new StringBuilder()
-            .append(`<!DOCTYPE html>`)
-            .append(`<html lang="en">`)
-            .append(`  <head>`)
-            // ToDo: get title from page.
-            .append(`    <title></title>`)
-            .appendGroup(
-              cssAsset => `    <link rel="stylesheet" href="/${cssAsset}" />`,
-              Array.from(meta.importedCss)
-            )
-            .appendGroup(
-              asset => `    <link rel="preload" href="/${asset}" as="style" />`,
-              Array.from(meta.importedAssets)
-            )
-            .append(`  </head>`)
-            .append()
-            .append(`  <body>`)
-            .append(`    <div id="#app">`)
-            // .append(`      ${htmlContent}`, !!htmlContent)
-            .append(`    </div>`)
-            .append()
-            .append(
-              `    <script type="module" src="/${chunk.fileName}"></script>`,
-              !!chunk.fileName
-            )
-            .append(`  </body>`)
-            .append(`</html>`)
-            .build()
+          source: html
         });
       }
 
