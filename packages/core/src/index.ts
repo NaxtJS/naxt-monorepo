53f2112d7f2d9825e95bd4f80e3d8e56abf4bb9b
MDReal32 <veysaliyev00@gmail.com>
2022-07-14 12:25:24 +0400

    chore: export watcher from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,5 +6,6 @@ export const defineConfig = (config: PartialDeep<AppConfig>) => config;
 export { Naxt } from "./core/naxt";
 export { Config } from "./core/config";
 export { Logger } from "./core/logger";
+export { Watcher } from "./core/watcher";
 export { ModuleGraph } from "./core/moduleGraph";
 export { defaultAppConfig } from "./const/defaultAppConfig";
