8a8a6daf256a837de28f06c4f82861b294737cfd
MDReal32 <veysaliyev00@gmail.com>
2022-07-14 12:24:30 +0400

    chore: on config input file enabling library mode

--- packages/rollup/src/index.ts
+++ packages/rollup/src/index.ts
@@ -1,26 +1,74 @@
-import { rollup, RollupOptions } from "rollup";
-import { config, Path, PluginHelper, resolvePlugins } from "@naxt/runtime";
+import { OutputOptions, rollup, RollupOptions } from "rollup";
+import { config, getPages, Path, PluginHelper, resolvePlugins } from "@naxt/runtime";
 import rimraf from "rimraf";
 
-const resolveOptions = (pages: Path<any>[], isBuild: boolean): RollupOptions => ({
-  input: pages.map(PluginHelper.transformToInputFile),
-  output: {
-    format: "esm",
-    entryFileNames(chunkInfo) {
-      return PluginHelper.cleanInputFile(chunkInfo.facadeModuleId).extension.set("[hash].js")
-        .importPath;
-    },
-    chunkFileNames: "assets/[name].[hash].js",
-    assetFileNames: "assets/[name].[hash].[ext]",
-    sourcemap: isBuild
-  },
-  plugins: resolvePlugins()
+declare module "rollup" {
+  interface OutputOptions {
+    nonAssetFile?: string;
+  }
+}
+
+interface ResolveOptions {
+  pages: Path<any>[];
+  outputOptions?: OutputOptions | OutputOptions[];
+  isBuild: boolean;
+  isDev: boolean;
+  isLibrary: boolean;
+}
+
+const resolveOptions = ({
+  pages,
+  outputOptions,
+  isBuild,
+  isDev,
+  isLibrary
+}: ResolveOptions): RollupOptions => ({
+  input: isLibrary
+    ? pages.map(page => page.fullPath)
+    : pages.map(PluginHelper.transformToInputFile),
+  onwarn(warning, handler) {},
+  output: outputOptions
+    ? outputOptions
+    : {
+        format: "esm",
+        entryFileNames: chunkInfo =>
+          PluginHelper.cleanInputFile(chunkInfo.facadeModuleId).extension.set(
+            `${isDev ? "" : "[hash]"}.js`
+          ).importPath,
+        chunkFileNames: `assets/[name]${isDev ? "" : ".[hash]"}.js`,
+        assetFileNames: `assets/[name]${isDev ? "" : ".[hash]"}.[ext]`,
+        nonAssetFile: `[name]${isDev ? "" : ".[hash]"}.[ext]`,
+        sourcemap: isBuild
+      },
+  plugins: resolvePlugins({ isLibrary })
 });
 
-export const bundle = async (pages: Path<any>[], outputDir?: string) => {
-  const { appConfig, isBuild } = config.getConfigs(["appConfig", "isBuild"]);
+export const bundle = async (outputDir?: string) => {
+  const {
+    appConfig,
+    appConfig: { input },
+    isBuild,
+    isDev
+  } = config.getConfigs(["appConfig", "isBuild", "isDev"]);
   outputDir = outputDir || appConfig.build.dir;
-  const rollupOptions = resolveOptions(pages, isBuild);
+
+  const isLibrary = !!input;
+  const pages = isLibrary
+    ? (Array.isArray(input)
+        ? input
+        : typeof input === "object"
+        ? Object.values(input)
+        : [input]
+      ).map(item => Path.from(item, config.getConfig("appRoot")))
+    : getPages({ path: true, absolute: true });
+  const rollupOptions = resolveOptions({
+    pages,
+    outputOptions: appConfig.output,
+    isBuild,
+    isDev,
+    isLibrary
+  });
+
   const bundle = await rollup(rollupOptions);
   const outputOptions = Array.isArray(rollupOptions.output)
     ? rollupOptions.output
